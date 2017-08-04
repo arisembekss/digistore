414aa8042c9dc642ff941c3dd13ac426c42285a1
achmad.aris30@gmail.com <aris300312>
2017-08-04 20:23:22 +0700

    regis firebase fix

--- application/views/v_redirect.php
+++ application/views/v_redirect.php
@@ -10,5 +10,34 @@
 	
 </head>
 <body>
-	<?php echo $nama;?>
+	<?php //echo $nama;?>
+	<input type="hidden" name="email" id="email" value="<?php echo $email;?>">
+	<input type="hidden" name="pass" id="pass" value="<?php echo $pass;?>">
+
+	<script src="https://www.gstatic.com/firebasejs/4.2.0/firebase.js"></script>
+	<script>
+	  // Initialize Firebase
+	  initFirebase();
+	</script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			var email = $("input#email").val();
+			var password = $("input#pass").val();
+			//alert(email);
+			firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+	        // Handle Errors here.
+	        var errorCode = error.code;
+	        var errorMessage = error.message;
+	        // [START_EXCLUDE]
+	        if (errorCode == 'auth/weak-password') {
+	          alert('The password is too weak.');
+	        } else {
+	          alert(errorCode);
+	        }
+	        console.log(error);
+	        // [END_EXCLUDE]
+	      });
+		});
+	</script>
 </body>
